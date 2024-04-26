9beb9a15f98d11cc16e3b015bb2ba2464fad4851
Admin-HL <yeonghaoliang0822@gmail.com>
2024-04-26 12:18:53 +0800

    Constructors Challenge - Wall

---
+++ src/AllChallenges/ConstructorChallenge/MainWall.java
@@ -0,0 +1,13 @@
+package AllChallenges.ConstructorChallenge;
+
+public class MainWall {
+
+    public static void main(String[] args) {
+        Wall wall = new Wall(5,4);
+        wall.setHeight(1.5);
+        wall.setWidth(11.5);
+        System.out.println("width = " + wall.getWidth());
+        System.out.println("height = " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
+    }
+}
---
+++ src/AllChallenges/ConstructorChallenge/Wall.java
@@ -0,0 +1,54 @@
+package AllChallenges.ConstructorChallenge;
+
+public class Wall {
+    private double width;
+    private double height;
+
+    public Wall (double width, double height){
+        if (width < 0) {
+            this.width = 0; // Set width to 0 if parameter is less than 0
+        } else {
+            this.width = width; // Set width to parameter value if it's non-negative
+        }
+
+        if (height < 0) {
+            this.height = 0; // Set height to 0 if parameter is less than 0
+        } else {
+            this.height = height; // Set height to parameter value if it's non-negative
+        }
+    }
+
+    public Wall() {
+        this.width = 0;
+        this.height = 0;
+    }
+
+    public double getWidth(){
+        return width;
+    }
+
+    public double getHeight(){
+        return height;
+    }
+
+    public void setWidth(double width){
+        if(width < 0){
+            this.width = 0;
+        }else{
+            this.width = width;
+        }
+    }
+
+    public void setHeight(double height){
+        if(height < 0){
+            this.height = 0;
+        }else{
+            this.height = height;
+        }
+    }
+
+    public double getArea(){
+        return this.width * this.height;
+    }
+
+}
