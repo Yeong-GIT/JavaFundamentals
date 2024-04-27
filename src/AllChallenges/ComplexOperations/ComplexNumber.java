38dbd07a5e92775e022e9b897f520566fb4decc1
Admin-HL <yeonghaoliang0822@gmail.com>
2024-04-27 21:06:17 +0800

    Complex Number - Constructor Challenge

---
+++ src/AllChallenges/ComplexOperations/ComplexNumber.java
@@ -0,0 +1,39 @@
+package AllChallenges.ComplexOperations;
+
+public class ComplexNumber {
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary){
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal(){
+        return this.real;
+    }
+
+    public double getImaginary(){
+        return this.imaginary;
+    }
+
+    public void add(double real, double imaginary){
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void add(ComplexNumber complexNumber){
+        this.real += complexNumber.real;
+        this.imaginary += complexNumber.imaginary;
+    }
+
+    public void subtract(double real, double imaginary){
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber number) {
+        this.real -= number.real;
+        this.imaginary -= number.imaginary;
+    }
+}
