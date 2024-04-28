ada881dddef125fcf4456667b680832948720741
Admin-HL <yeonghaoliang0822@gmail.com>
2024-04-29 01:25:31 +0800

    Inheritance - Dog

--- src/Inheritance/Dog.java
+++ src/Inheritance/Dog.java
@@ -1,7 +1,42 @@
 package Inheritance;
 
 public class Dog extends Animal{
+
+    private String earShape;
+    private String tailShape;
+
+
     public Dog(){
         super("K9", "Big", 90);
+    }
+
+    public Dog(String type, double weight){
+        this(type, weight, "Perky", "Curled");
+    }
+
+    public Dog(String type, double weight, String earShape, String tailShape) {
+        super(type, weight < 15 ? "small" : (weight < 35 ? "medium" : "large"), weight);
+        this.earShape = earShape;
+        this.tailShape = tailShape;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "earShape='" + earShape + '\'' +
+                ", tailShape='" + tailShape + '\'' +
+                "} " + super.toString();
+    }
+
+    @Override
+    public void makeNoise() {
+        super.makeNoise();
+        System.out.println("If it makes big noise, it is in big size");
+    }
+
+    @Override
+    public void move(String speed) {
+        super.move(speed);
+        System.out.println("Dogs walk, run and wag their tail.");
     }
 }
--- src/Inheritance/Main.java
+++ src/Inheritance/Main.java
@@ -4,6 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Animal animal = new Animal ("Mutt", "big", 70);
         doSomething(animal, "slow");
+
+        Dog dog = new Dog();
+        doSomething(dog, "fast");
+
+        Dog yorkie = new Dog("Yorkie", 15);
+        doSomething(yorkie, "fast");
+
+        Dog retriever = new Dog("Retriever White", 65, "Floppy", "Swimmer");
+        doSomething(retriever, "slow");
     }
 
     public static void doSomething(Animal animal, String speed){
